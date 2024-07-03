2d225d95658672ed90b24feed38ad65cff9f3eda
tctienn <mainichichimvm@gmail.com>
2024-07-03 09:33:20 +0700

    vẫn lỗi draw

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
+// import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import Home from './home/index'
+// // import as from './(tabs)/as2'
+// const Drawer = createDrawerNavigator();
+
+// export default function Layout() {
+//   return (
+//     <GestureHandlerRootView style={{ flex: 1 }}>
+//       <Drawer.Navigator>
+//         <Drawer.Screen
+//           name="Home" // Tên trang và phải khớp với url từ gốc
+//           options={{
+//             drawerLabel: 'Home', // Nhãn hiển thị trong menu
+//             title: 'Overview', // Tiêu đề trang
+//           }}
+//           component={Home} // Thay HomeScreen bằng màn hình bạn muốn dẫn đến
+//         />
+
+
+//       </Drawer.Navigator>
+//     </GestureHandlerRootView>
+//   );
+// }
+
+
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// import { Drawer } from 'expo-router/drawer';
+
+import Ay from './Demo2'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from './home/index'
 const Drawer = createDrawerNavigator();
 
 export default function Layout() {
@@ -8,12 +36,12 @@ export default function Layout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer.Navigator>
         <Drawer.Screen
-          name="(tabs)" // Tên trang và phải khớp với url từ gốc
+          name="Demo2" // Tên trang và phải khớp với url từ gốc
           options={{
             drawerLabel: 'Home', // Nhãn hiển thị trong menu
             title: 'Overview', // Tiêu đề trang
           }}
-          component={Home} // Thay HomeScreen bằng màn hình bạn muốn dẫn đến
+          component={Ay} // Thay HomeScreen bằng màn hình bạn muốn dẫn đến
         />
 
 
